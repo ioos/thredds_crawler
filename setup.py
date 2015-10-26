3b0ce2df75f01e00aceb8d559d5f2d3351e06e22
Kyle Wilcox <kyle@axiomalaska.com>
2015-10-26 10:22:58 -0400

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,15 @@
 from __future__ import with_statement
-import sys
 
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
 
 from thredds_crawler import __version__
+
 
 def readme():
     with open('README.md') as f:
         return f.read()
 
 reqs = [line.strip() for line in open('requirements.txt')]
-
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-    def run_tests(self):
-        import pytest
-        errno = pytest.main(self.test_args)
-        sys.exit(errno)
 
 setup(
     name                = "thredds_crawler",
@@ -29,18 +18,16 @@ setup(
     long_description    = readme(),
     license             = 'GPLv3',
     author              = "Kyle Wilcox",
-    author_email        = "kwilcox@sasascience.com",
+    author_email        = "kyle@axiomdatascience.com",
     url                 = "https://github.com/asascience-open/thredds_crawler",
     packages            = find_packages(),
     install_requires    = reqs,
-    tests_require       = ['pytest'],
-    cmdclass            = {'test': PyTest},
     classifiers         = [
-            'Development Status :: 4 - Beta',
+            'Development Status :: 5 - Production/Stable',
             'Intended Audience :: Developers',
             'Intended Audience :: Science/Research',
             'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-            'Operating System :: POSIX :: Linux',
+            'Operating System :: OS Independent',
             'Programming Language :: Python',
             'Topic :: Scientific/Engineering',
         ],
